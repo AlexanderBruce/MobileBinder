230c36b9f5db0ce283d2a67fdc3d74cd506a27f5
Sam Rang <srang2010@gmail.com>
2013-02-01 12:42:01 -0500

    payroll view controller

--- MobileBinder/PayrollNotificationsViewController.m
+++ MobileBinder/PayrollNotificationsViewController.m
@@ -106,13 +106,11 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-    // Return the number of sections.
     return [self.notificationSettingsSectionsAndRows.allKeys count];
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    // Return the number of rows in the section.
     NSString *key = [self.notificationSettingsSectionsAndRows.allKeys objectAtIndex:section];
     return [[self.notificationSettingsSectionsAndRows objectForKey:key]count];
 }
@@ -127,6 +125,7 @@
     UILabel *title = [[UILabel alloc]initWithFrame:labelFrame];
     NSString *key = [self.notificationSettingsSectionsAndRows.allKeys objectAtIndex:indexPath.section];
     title.text = [[self.notificationSettingsSectionsAndRows objectForKey:key]objectAtIndex:indexPath.row];
+    title.backgroundColor = [UIColor clearColor];
     [cell addSubview:title];
     
     CGRect switchFrame = CGRectMake(220, 10, 40, 22);
@@ -142,59 +141,4 @@
     return [self.notificationSettingsSectionsAndRows.allKeys objectAtIndex:section];
 }
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    }   
-    else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-#pragma mark - Table view delegate
-
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Navigation logic may go here. Create and push another view controller.
-    /*
-     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
-     // ...
-     // Pass the selected object to the new view controller.
-     [self.navigationController pushViewController:detailViewController animated:YES];
-     */
-}
-
-- (void)viewDidUnload {
-    [super viewDidUnload];
-}
 @end
---
+++ MobileBinder/PayrollPeriodViewController.h
@@ -0,0 +1,13 @@
+//
+//  PayrollPeriodViewController.h
+//  MobileBinder
+//
+//  Created by Samuel Rang on 1/30/13.
+//  Copyright (c) 2013 Duke University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PayrollPeriodViewController : UITableViewController
+
+@end
