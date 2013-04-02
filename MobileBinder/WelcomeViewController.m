72a010f0829bf4810e0ad30422c2c1b1b5ca9d19
Andrew Patterson <andrew.patterson@duke.edu>
2013-04-01 23:36:06 -0400

    changes

--- MobileBinder/WelcomeViewController.m
+++ MobileBinder/WelcomeViewController.m
@@ -71,8 +71,8 @@
 {
     if(section == WELCOME_SECTION) return 0;
     else if (section == RECOMMEND_SECTION) return 2;
-//    else if (section ==)
-//    else return 1;
+    else if (section == OPTIONAL_SECTION) return 1;
+    else return 1;
 }
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
@@ -83,7 +83,8 @@
 - (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
 {
     if(section == WELCOME_SECTION) return WELCOME_HEADER;
-//    else if(
+    else if(section == RECOMMEND_SECTION)return RECOMMEND_HEADER;
+    else if(section == OPTIONAL_SECTION) return OPTIONAL_HEADER;
     else return @"";
 }
 
